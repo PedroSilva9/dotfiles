632f2c54136ac41107bce94cf139715efb4af644
Pedro Silva <ojosepedro@gmail.com>
2020-02-09 15:11:16 +0000

    Updated link script

--- linking.sh
+++ linking.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
-ln -sf ~/dotfiles/i3/config ~/.config/i3/config
-ln -sf ~/dotfiles/.zshrc ~/.zshrc
-ln -sf ~/dotfiles/.vimrc ~/.vimrc
-ln -sf ~/dotfiles/termite/config ~/.config/termite/config
-ln -sf ~/dotfiles/nvim/init.vim ~/.config/nvim/init.vim
+ln -sfT ~/.dotfiles/.zshrc ~/.zshrc
+ln -sfT ~/.dotfiles/i3/config ~/.config/i3/config
+ln -sfT ~/.dotfiles/nvim/init.vim ~/.config/nvim/init.vim
+ln -sfT ~/.dotfiles/termite/config ~/.config/termite/config
