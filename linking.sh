acc465bdd3cdccdff148b6e8e48cd98900fc44ee
Pedro Silva <ojosepedro@gmail.com>
2020-02-09 15:18:18 +0000

    Links are now correct

--- linking.sh
+++ linking.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-ln -sfT ~/.dotfiles/.zshrc ~/.zshrc
-ln -sfT ~/.dotfiles/i3/config ~/.config/i3/config
-ln -sfT ~/.dotfiles/nvim/init.vim ~/.config/nvim/init.vim
-ln -sfT ~/.dotfiles/termite/config ~/.config/termite/config
+ln -sf ~/.dotfiles/.zshrc ~/.zshrc
+ln -sf ~/.dotfiles/.config/i3/config ~/.config/i3/config
+ln -sf ~/.dotfiles/.config/nvim/init.vim ~/.config/nvim/init.vim
+ln -sf ~/.dotfiles/.config/termite/config ~/.config/termite/config
