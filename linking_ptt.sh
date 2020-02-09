61feb283bdb04fcaddd33503f453de10c5471e49
Pedro Silva <ojosepedro@gmail.com>
2020-02-09 15:21:13 +0000

    Removed ppt linking script

--- linking_ptt.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-ln -sf ~/dotfiles/i3/config_ptt ~/.i3/config
-ln -sf ~/dotfiles/.zshrc ~/.zshrc
-ln -sf ~/dotfiles/.vimrc ~/.vimrc
-ln -sf ~/dotfiles/termite/config ~/.config/termite/config
-#ln -sf ~/dotfiles/nvim/init.vim ~/.config/nvim/init.vim
