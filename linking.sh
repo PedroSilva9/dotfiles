3c0da252c1323a38b7d1caaea9a9057f4a908ed8
Pedro Silva <ojosepedro@gmail.com>
2020-08-23 23:20:39 +0000

    Fixed link script! Removed i3wm and added bspwm

--- linking.sh
+++ linking.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 ln -sf ~/.dotfiles/.zshrc ~/.zshrc
-ln -sf ~/.dotfiles/.config/i3/config ~/.config/i3/config
+#ln -sf ~/.dotfiles/.config/i3/config ~/.config/i3/config
 ln -sf ~/.dotfiles/.config/nvim/init.vim ~/.config/nvim/init.vim
 ln -sf ~/.dotfiles/.config/termite/config ~/.config/termite/config
+ln -sf ~/.dotfiles/.config/bspwm/bspwmrc ~/.config/bspwm/bspwmrc
+ln -sf ~/.dotfiles/.config/sxhkd/sxhkdrc ~/.config/sxhkd/sxhkdrc
+ln -sf ~/.dotfiles/.config/polybar/config ~/.config/polybar/config
+ln -sf ~/.dotfiles/.config/polybar/launch.sh ~/.config/polybar/launch.sh
